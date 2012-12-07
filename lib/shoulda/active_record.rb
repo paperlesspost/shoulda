d8542d9647f24243d5eb960d55604a14040ac44a
Aaron Quint <aaron@quirkey.com>
2012-12-07 18:06:39 -0500

    Fix AR:Macros

--- lib/shoulda/active_record.rb
+++ lib/shoulda/active_record.rb
@@ -7,4 +7,4 @@ require 'shoulda/active_record/macros'
 ActiveSupport::TestCase.send :include, Shoulda::ActiveRecord::Helpers
 ActiveSupport::TestCase.send :include, Shoulda::ActiveRecord::Matchers
 ActiveSupport::TestCase.send :include, Shoulda::ActiveRecord::Assertions
-ActiveSupport::TestCase.send :extend, Shoulda::ActionController::Macros
+ActiveSupport::TestCase.send :extend, Shoulda::ActiveRecord::Macros
